3a03452a16a3f823070699431343dae4c59fc791
Froyo101 <jakehasmail@yahoo.com>
2021-08-10 10:43:01 -0500

    Made minor style adjustments

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -60,6 +60,7 @@ class FriendsController < ApplicationController
     end
   end
 
+  # Identifies whether or not current user is the owner of a friend listing with full privileges
   def correct_user
     @friend = current_user.friends.find_by(id: params[:id])
     redirect_to friends_path, notice: "Not Authorized To Edit This Friend" if @friend.nil?
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,5 +3,6 @@ class HomeController < ApplicationController
   end
 
   def about
+    @about_me = "My name is Jake Foiles, and I'm a software developer currently based in Illinois!"
   end
 end
